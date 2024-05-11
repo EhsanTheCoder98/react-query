1eaac01553c4a8cd85636afe00892f961526ef93
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2024-05-11 23:21:42 +0330

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {  QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Homepage from './components/Homepage';
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <Homepage />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 };
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
 
 const Homepage = () => {
+  const queryKey = ["users"];
+  const queryFn = () => {
+    return fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
+      res.json()
+    );
+  };
+  const { isPending, error, data } = useQuery({ queryKey, queryFn });
+  console.log({ data });
+  if (isPending) return <h1>LOADING...</h1>;
     return (
-        <div>
-          home  
-        </div>
+      <div>
+        {data.map((i) => {
+            return <h3 key={i.id}>{i.name}</h3>;
+          })}
+      </div>
     );
 };
 
