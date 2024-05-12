d133623bc796ae10611bcb407ad3e18fcd947dec
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2024-05-12 20:34:03 +0330

    cacheTime,staleTime,refetch,onSuccess,onError

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,16 @@ import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries:{
+      gcTime:100000,
+      staleTime:100000,
+      refetchOnMount:false,
+      refetchOnWindowFocus:false,
+    }
+  }
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}> 
